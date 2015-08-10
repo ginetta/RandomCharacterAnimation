92ba32a4017f243c66da5198d9c35c733b05e49a
vanderlanth <nicolaslanthemann@gmail.com>
2015-08-10 09:47:48 +0200

    minify + autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var jade = require('gulp-jade');
+var uglify = require('gulp-uglify');
+var minifyCss = require('gulp-minify-css');
+var minifyHTML = require('gulp-minify-html');
 var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
 
@@ -23,21 +26,30 @@ gulp.task('sass', function() {
   return gulp.src('src/*.scss')
     .pipe(sass())
     .pipe(autoprefixer({
-            cascade: false
+        cascade: false
      }))
+    .pipe(minifyCss({compatibility: 'ie8'}))
     .pipe(gulp.dest('build/'))
     .pipe(browserSync.stream());
 });
 
 gulp.task('jade', function() {
+
+    var opts = {
+        conditionals: true,
+        spare:true
+    };
+
      return gulp.src('src/*.jade')
     .pipe(jade())
+    .pipe(minifyHTML(opts))
     .pipe(gulp.dest('build/'))
     .pipe(browserSync.stream());
 });
 
 gulp.task('scripts', function() {
      return gulp.src('src/*.js')
+    .pipe(uglify())
     .pipe(gulp.dest('build/'))
     .pipe(browserSync.stream());
 });
